7ea5db76d8cb2403e5dd8adb4fe00de87503ab34
Reon0416 <re16man@icloud.com>
2025-07-02 21:24:00 +0900

    仮完成

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import InputPage from "./pages/InputPage";
 import ResultPage from "./pages/ResultPage";
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
 
           <Route path="/" element={<Navigate to="/input" />} />
           <Route path="/input" element={<InputPage />} />
           <Route path="/result" element={<ResultPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 };
